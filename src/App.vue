805bec974b6e3d390ede24623026fe030bea101c
SilavongSone <silavongsone@gmail.com>
2023-02-03 09:50:09 +0700

    dashbord-nav-bar

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="inline-block bg-fuchsia-100 h-screen w-screen">
+    <div class="h-screen w-full flex fixed top-0">
+      <section class="h-full w-48">
+        <div class="h-full">
+          <iconNavbar> </iconNavbar>
+        </div>
+      </section>
+
+      <section class="h-full w-screen left-44">
+        <div>
+          <profile></profile>
+        </div>
+      </section>
+      <section class="relative left-80 top-80">helllo</section>
+    </div>
+
+    <div>
+      <section>
+        <div class="pl-28 pt-4 m-20 w-fit h-full">
+          <conten></conten>
+        </div>
+      </section>
+    </div>
+  </section>
+
+</template>
+
+<script setup>
+import iconNavbar from "./components/iconNavbar.vue";
+import profile from "./components/profile.vue";
+import conten from "./components/conten.vue";
+</script>
+
+<style scoped></style>
---
+++ src/components/iconNavbar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="w-full h-full flex">
+    <section class="bg-white w-44 h-full text-xs">
+      <div class="flex items-center justify-center">
+        <img class="p-2 w-20" src="../assets/logo/logo.png" alt="logo" />
+      </div>
+      <ul class="w-36 text-gray-500 p-2">
+        <li
+          class="w-40 items-center flex cursor-pointer hover:bg-violet-600 hover:text-white rounded-md"
+        >
+          <img class="w-10 p-2" src="../assets/icon/dashboard.png" />
+          Dashboard
+        </li>
+        <li
+          class="w-40 items-center flex cursor-pointer hover:bg-violet-600 hover:text-white rounded-md"
+        >
+          <img class="w-10 p-2" src="../assets/icon/online-course.png" />
+          Course
+        </li>
+        <li
+          class="w-40 items-center flex cursor-pointer hover:bg-violet-600 hover:text-white rounded-md"
+        >
+          <img class="w-10 p-2" src="../assets/icon/teacher.png" />
+          Teacher
+        </li>
+        <li
+          class="w-40 items-center flex cursor-pointer hover:bg-violet-600 hover:text-white rounded-md"
+        >
+          <img class="w-10 p-2" src="../assets/icon/timetable.png" />
+          Timetable
+        </li>
+        <li
+          class="w-40 items-center flex cursor-pointer hover:bg-violet-600 hover:text-white rounded-md"
+        >
+          <img class="w-10 p-2" src="../assets/icon/result.png" />
+          Exam
+        </li>
+        <li
+          class="w-40 items-center flex cursor-pointer hover:bg-violet-600 hover:text-white rounded-md"
+        >
+          <img class="w-10 p-2" src="../assets/icon/homework.png" />
+          Homework
+        </li>
+        <li
+          class="w-40 items-center flex cursor-pointer hover:bg-violet-600 hover:text-white rounded-md"
+        >
+          <img class="w-10 p-2" src="../assets/icon/messenger.png" />
+          Message
+        </li>
+        <li
+          class="w-40 items-center flex cursor-pointer hover:bg-violet-600 hover:text-white rounded-md"
+        >
+          <img class="w-10 p-2" src="../assets/icon/friendship.png" />
+          Friends
+        </li>
+        <li></li>
+        <li></li>
+      </ul>
+    </section>
+
+    <section></section>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
